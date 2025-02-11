ff9bbc1f123361b7b44fd310188b0ae5951b2f04
Matthew Da Silva <matthewgdasilva25@gmail.com>
2025-02-11 10:18:22 +0000

    add basic template and routing for explore habits page

---
+++ habit_tracker/urls.py
@@ -0,0 +1,6 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('explore/', views.BrowseHabits.as_view(), name='browse_habits'),
+]
--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Habit, CheckIn
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Hello, World!')
+
+class BrowseHabits(generic.ListView):
+    queryset = Habit.objects.all().filter(visibility=2)
+    template_name = "habit_tracker/explore_habits.html"
+    paginate_by = 5
+    
