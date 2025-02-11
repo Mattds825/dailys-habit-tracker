28eeb0fa5a75b0ffe2b8d04bdd0b19ddac83eba8
Matthew Da Silva <matthewgdasilva25@gmail.com>
2025-02-11 17:14:26 +0000

    add view for user_habits page that opens habits assosciated to a specific user

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('explore/', views.BrowseHabits.as_view(), name='explore'),
+    path('<str:user>/', views.user_habits, name='user_habits'),
 ]
--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views import generic
 from .models import Habit, CheckIn
 
@@ -8,4 +8,28 @@ class BrowseHabits(generic.ListView):
     queryset = Habit.objects.all().filter(visibility=2)
     template_name = "habit_tracker/explore_habits.html"
     paginate_by = 5
+
+def user_habits(request, user):
+    """
+    display posts associated with a user
+    show add post button if user is logged in
+    """
     
+    # print(request.user)
+    
+    queryset = Habit.objects.all()
+    habits = [h for h in queryset if h.user.username == user]
+   
+    # get check-ins for each habit with dates
+    for h in habits:
+        if h.check_ins.all():
+            for c in h.check_ins.all():
+                print(c.checked_in_on.date())
+            
+    
+    # user = get_list_or_404(queryset, user=user)
+    return render(request, 'habit_tracker/user_habits.html', 
+                  {'habits': habits,
+                   'user': user
+                   }
+            )    
